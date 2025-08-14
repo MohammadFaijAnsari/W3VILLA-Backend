f75b078b0c393d600587bf29d396c6849d6e5a5a
NohammadFaijAnsai <mohammadfaijansari6@gmail.com>
2025-08-14 10:46:24 +0530

    connect atlas

--- MERN-CRUD/backend/database.js
+++ MERN-CRUD/backend/database.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 const databaseConnection =async ()=>{
-    mongoose.connect("mongodb://localhost:27017/")
+    mongoose.connect("mongodb+srv://mohammadfaijansari6:FaijAnsari99720@cluster0.dx8qmzp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(()=>{
        console.log("MongoDB Connect Sucessfully");
     }).catch((err)=>{
