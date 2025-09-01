decd7fd707a2bb08f31d28b16cbde2c8ea146423
NohammadFaijAnsai <mohammadfaijansari6@gmail.com>
2025-09-01 23:35:15 +0530

    Update

--- Youtube-Blog/db.connect.js
+++ Youtube-Blog/db.connect.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/blogify')
+// mongoose.connect('mongodb://localhost:27017/blogify')
+mongoose.connect("mongodb+srv://mohammadfaijansari6:FaijAnsari99720@cluster0.dx8qmzp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(e=>console.log("MongoDB Connected Sucessfully"))
 
 module.exports=mongoose;
